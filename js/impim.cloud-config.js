cee21f40f6c613d9af8164c8797c2f41ccbb354d
Guilherme Cirne <gcirne@gmail.com>
2012-10-16 17:27:21 -0300

    thumbor unsafe url config.

--- js/impim.cloud-config.js
+++ js/impim.cloud-config.js
@@ -2,6 +2,7 @@
     window.impim = window.impim || {};
     window.impim.config = {
         images_url: 'http://ec2-174-129-53-130.compute-1.amazonaws.com:8080/alpha/images.js',
-        thumbor_urls_url: 'http://ec2-174-129-53-130.compute-1.amazonaws.com:8080/thumbor_urls'
+        thumbor_urls_url: 'http://ec2-174-129-53-130.compute-1.amazonaws.com:8080/thumbor_urls',
+        thumbor_unsafe_url: 'http://ec2-23-23-19-206.compute-1.amazonaws.com:8888/unsafe'
     };
 })(window);
