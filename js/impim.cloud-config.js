4449e338a1d122b3137973e571d60f22e37ec733
Guilherme Cirne <gcirne@gmail.com>
2012-10-16 16:08:19 -0300

    Initial commit.

---
+++ js/impim.cloud-config.js
@@ -0,0 +1,7 @@
+(function(window) {
+    window.impim = window.impim || {};
+    window.impim.config = {
+        images_url: 'http://ec2-174-129-53-130.compute-1.amazonaws.com:8080/alpha/images.js',
+        thumbor_urls_url: 'http://ec2-174-129-53-130.compute-1.amazonaws.com:8080/thumbor_urls.js'
+    };
+})(window);
