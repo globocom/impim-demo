6bf43ff22f12b17d4780e08e11362a04e65a0b12
Guilherme Cirne <gcirne@gmail.com>
2012-10-17 17:33:59 -0300

    new API url.

--- js/impim.cloud-config.js
+++ js/impim.cloud-config.js
@@ -1,8 +1,8 @@
 (function(window) {
     window.impim = window.impim || {};
     window.impim.config = {
-        imagesUrl: 'http://ec2-174-129-53-130.compute-1.amazonaws.com:8080/alpha/images.js',
-        thumborUrlsUrl: 'http://ec2-174-129-53-130.compute-1.amazonaws.com:8080/thumbor_urls',
+        imagesUrl: 'http://ec2-184-72-215-237.compute-1.amazonaws.com:8080/alpha/images.js',
+        thumborUrlsUrl: 'http://ec2-184-72-215-237.compute-1.amazonaws.com:8080/thumbor_urls',
         thumborUnsafeUrl: 'http://ec2-23-23-19-206.compute-1.amazonaws.com:8888/unsafe'
     };
 })(window);
