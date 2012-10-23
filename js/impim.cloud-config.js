c5e8c8649531ee753993a5850f8d4b48ef8b4c2d
Guilherme Cirne <gcirne@gmail.com>
2012-10-23 13:11:54 -0200

    imagesUrl with no trailing .js.

--- js/impim.cloud-config.js
+++ js/impim.cloud-config.js
@@ -1,7 +1,7 @@
 (function(window) {
     window.impim = window.impim || {};
     window.impim.config = {
-        imagesUrl: 'http://ec2-184-72-215-237.compute-1.amazonaws.com:8080/alpha/images.js',
+        imagesUrl: 'http://ec2-184-72-215-237.compute-1.amazonaws.com:8080/alpha/images',
         thumborUrlsUrl: 'http://ec2-184-72-215-237.compute-1.amazonaws.com:8080/thumbor_urls',
         thumborUnsafeUrl: 'http://ec2-23-23-19-206.compute-1.amazonaws.com:8888/unsafe'
     };
