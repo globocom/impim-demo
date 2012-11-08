9d74e89253ace8189a45db405399e8d954432b69
Guilherme Cirne <gcirne@gmail.com>
2012-11-08 14:13:45 -0200

    new impim-api host.

--- js/impim.cloud-config.js
+++ js/impim.cloud-config.js
@@ -1,8 +1,8 @@
 (function(window) {
     window.impim = window.impim || {};
     window.impim.config = {
-        imagesUrl: 'http://ec2-184-72-215-237.compute-1.amazonaws.com:8080/alpha/images',
-        thumborUrlsUrl: 'http://ec2-184-72-215-237.compute-1.amazonaws.com:8080/thumbor_urls',
+        imagesUrl: 'http://ec2-54-242-113-92.compute-1.amazonaws.com:8080/alpha/images',
+        thumborUrlsUrl: 'http://ec2-54-242-113-92.compute-1.amazonaws.com:8080/thumbor_urls',
         thumborUnsafeUrl: 'http://ec2-23-23-19-206.compute-1.amazonaws.com:8888/unsafe'
     };
 })(window);
